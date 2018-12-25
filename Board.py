1482f1c656cd86289bce1ec23a1ea9b2b903d0db
Alexander Dunlap <alexander.dunlap@gmail.com>
2018-12-25 14:15:43 -0800

    finishing show function

--- Board.py
+++ Board.py
@@ -1,3 +1,5 @@
+from ansi.colour import fg,bg
+
 class Board:
     def __init__(self,height,width,double_letter_scores,
             double_word_scores,triple_letter_scores,triple_word_scores,
@@ -5,16 +7,16 @@ class Board:
         self.height = height
         self.width = width
         self.double_letter_scores = double_letter_scores
-        self.double_letter_scores = double_word_scores
+        self.double_word_scores = double_word_scores
         self.triple_letter_scores = triple_letter_scores
         self.triple_word_scores = triple_word_scores
         self.starting_square = starting_square
 
 def make_official_scrabble_board():
-    triple_word_scores = [(0,0),(0,7),(0,14),(7,0)]
-    double_letter_scores = [(0,3),(0,11),(2,6),(2,8),(3,0),(3,7),(3,14),(6,2),(6,6),(6,8),(6,12),(7,3),(7,11),(7,14)]
-    double_word_scores = [(1,1),(1,14),(2,2),(2,13),(3,3),(3,12),(4,4),(4,11)]
-    triple_letter_scores = [(1,5),(1,9),(5,1),(5,5),(5,10),(5,13)]
+    triple_word_scores = [(0,0),(0,7),(0,14),(7,0),(7,14)]
+    double_letter_scores = [(0,3),(0,11),(2,6),(2,8),(3,0),(3,7),(3,14),(6,2),(6,6),(6,8),(6,12),(7,3),(7,11)]
+    double_word_scores = [(1,1),(7,7),(1,13),(2,2),(2,12),(3,3),(3,11),(4,4),(4,10)]
+    triple_letter_scores = [(1,5),(1,9),(5,1),(5,5),(5,9),(5,13)]
     for l in [triple_word_scores,double_letter_scores,double_word_scores,triple_letter_scores]:
         tmp_l = l[:]
         for j,(r,c) in enumerate(tmp_l):
@@ -28,3 +30,22 @@ class BoardState:
         self.played_letters = []
         for r in range(self.board.height):
             self.played_letters.append([' ']*self.board.width)
+    def show(self):
+        for r,row in enumerate(self.played_letters):
+            for c,letter in enumerate(row):
+                if (r,c) in self.board.triple_word_scores:
+                    f = bg.red
+                elif (r,c) in self.board.double_word_scores:
+                    f = bg.magenta
+                elif (r,c) in self.board.triple_letter_scores:
+                    f = bg.blue
+                elif (r,c) in self.board.double_letter_scores:
+                    f = bg.cyan
+                else:
+                    f = lambda x: x
+                if letter == ' ':
+                    print_letter = '\u3000'
+                else:
+                    print_letter = chr(0xff00 + ord(letter)-ord('A')+ord("!"))
+                print(f(print_letter),end="")
+            print()
